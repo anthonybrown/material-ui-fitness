51721b125617260e352e77b209725fe0c862ef11
Tony Brown <tony@tonybrown.me>
2018-05-04 08:44:26 -0400

    refactor code by setting up a MVC style setup, style components

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -1,18 +1,52 @@
-import React from 'react';
-import { Grid, Paper } from 'material-ui'
-import LeftPane from './LeftPane'
-import RightPane from './RightPane'
+import React, { Fragment } from 'react'
+import { Grid, Paper, Typography } from 'material-ui'
+import List, { ListItem, ListItemText  } from 'material-ui/List'
 
 const styles = {
-  Paper: { padding: 20, marginTop: 10, marginBottom: 10 }
+  Paper: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    height: 370,
+    overflowY: 'auto'
+  }
 }
 
-export default (props) =>
+export default ({ exercises }) =>
   <Grid container spacing={8}>
     <Grid item sm>
-      <LeftPane styles={styles} />
+      <Paper style={styles.Paper}>
+        {exercises.map(([group, exercises]) =>
+          <Fragment>
+            <Typography
+              variant='title'
+              style={{textTransform: 'capitalize'}}
+            >
+              {group}
+            </Typography>
+            <List component='ul'>
+              {exercises.map(({ title }) =>
+                <ListItem button>
+                  <ListItemText primary={title} />
+                </ListItem>
+              )}
+            </List>
+          </Fragment>
+        )}
+      </Paper>
     </Grid>
     <Grid item sm>
-      <RightPane styles={styles} />
+      <Paper style={styles.Paper}>
+        <Typography
+          variant='display1'
+        gutterBottom>
+          Welcome
+        </Typography>
+        <Typography
+          variant='subheading'
+        >
+          Please select an exercise from the list on the left.
+        </Typography>
+      </Paper>
     </Grid>
   </Grid>
--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Paper, Tabs } from 'material-ui'
 import { Tab } from 'material-ui/Tabs'
 
-export default (props) =>
-  <Paper>
+export default ({ muscles }) =>
+  <Paper style={{marginTop: 20}}>
     <Tabs
       value={0}
       indicatorColor='primary'
       textColor='primary'
       centered
     >
-      <Tab label="Item One" />
-      <Tab label="Item Two" />
-      <Tab label="Item Three" />
+      <Tab label='All' />
+      {muscles.map(group =>
+        <Tab label={group} />
+      )}
     </Tabs>
   </Paper>
