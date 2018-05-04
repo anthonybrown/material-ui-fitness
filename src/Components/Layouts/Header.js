c2f7298856df925855685902809c887ee3de2a82
Tony Brown <tony@tonybrown.me>
2018-05-04 12:00:16 -0400

    add modal so user can add new exercises

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from 'material-ui'
+import CreateDialog from '../Exercises/Dialogs/Create'
 
 export default (props) => (
   <AppBar position="static">
     <Toolbar>
-      <Typography variant="headline" color='inherit'>
-        Exercise Database
+      <Typography
+        style={{flex: 1}}
+        variant="headline"
+        color='inherit'
+      >
+        Exercise App
       </Typography>
+
+      <CreateDialog />
     </Toolbar>
   </AppBar>
 
